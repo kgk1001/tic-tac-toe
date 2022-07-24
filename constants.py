257f7033758ca3666163ff73a2852286f556324b
kgk1001 <kgk1001@outlook.com>
2022-07-25 00:40:52 +0530

    add new functions

--- constants.py
+++ constants.py
@@ -3,8 +3,13 @@ height = 600
 rows = 3
 cols = 3
 
-sqSize = width // cols # which is == 200
+sqSize = width // cols  # which is == 200
+radius = sqSize // 4
 lineWidth = 15
+offSet = 50
+
 # Colour
 bgColor = (150, 170, 150)
 lineColor = (190, 200, 190)
+circleColor = (190, 200, 100)
+crossColor = (90, 200, 190)
--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 # 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
 import sys
-
 import numpy
 import pygame
-
 from constants import *
 
 # pygame setup
@@ -17,13 +15,60 @@ class BOARD:
 
     def __init__(self):
         self.place = numpy.zeros((rows, cols))
+        self.empty_sqrs = self.place
+        self.marked_sqrs = 0
         print(self.place)
+
+    def final_states(self):
+
+        # vertical wins
+        for col in range(cols):
+            if self.place[0][col] == self.place[1][col] == self.place[2][col] != 0:
+                if show:
+                    color = CIRC_COLOR if self.squares[0][col] == 2 else CROSS_COLOR
+                    iPos = (col * SQSIZE + SQSIZE // 2, 20)
+                    fPos = (col * SQSIZE + SQSIZE // 2, HEIGHT - 20)
+                    pygame.draw.line(screen, color, iPos, fPos, LINE_WIDTH)
+                return self.place[0][col]
+
+        # horizontal wins
+        for row in range(rows):
+            if self.place[row][0] == self.place[row][1] == self.place[row][2] != 0:
+                if show:
+                    color = circleColor if self.place[row][0] == 2 else crossColor
+                    iPos = (20, row * sqSize + sqSize // 2)
+                    fPos = (width - 20, row * sqSize + sqSize // 2)
+                    pygame.draw.line(screen, color, iPos, fPos, lineWidth)
+                return self.place[row][0]
+
+        # desc diagonal
+        if self.place[0][0] == self.place[1][1] == self.place[2][2] != 0:
+            if show:
+                color = circleColor if self.place[1][1] == 2 else crossColor
+                iPos = (20, 20)
+                fPos = (width - 20, height - 20)
+                pygame.draw.line(screen, color, iPos, fPos, lineWidth)
+            return self.place[1][1]
 
     def mark_place(self, row, col, player):
         self.place[row][col] = player
+        self.marked_sqrs += 1
 
-    def IsPlaceFree(self, row, col):
+    def isPlaceFree(self, row, col):
         return self.place[row][col] == 0
+
+    def isFull(self):
+        return self.marked_sqrs == 9
+
+    def isEmpty(self):
+        return self.marked_sqrs == 0
+
+    def get_empty_sqrs(self):
+        empty_sqrs = []
+        for row in range(rows):
+            for col in range(cols):
+                if self.isEmpty(row, col):
+                    empty_sqrs.append(row, col)
 
 
 class Game:
@@ -32,6 +77,10 @@ class Game:
         self.board = BOARD()
         self.show_lines()
         self.player = 1
+        self.gamemode = 'ai'  # pvp or ai
+        self.running = True
+
+
 
     def show_lines(self):
         # vetical lines
@@ -41,8 +90,28 @@ class Game:
         pygame.draw.line(screen, lineColor, (0, sqSize), (width, sqSize), lineWidth)
         pygame.draw.line(screen, lineColor, (0, height - sqSize), (width, height - sqSize), lineWidth)
 
+    def draw_fig(self, row, col):
+        if self.player == 1:
+            # draw cross
+
+            desStart = (col * sqSize + offSet, row * sqSize + offSet)
+            desEnd = (col * sqSize + sqSize - offSet, row * sqSize + sqSize - offSet)
+            pygame.draw.line(screen, crossColor, desStart, desEnd, lineWidth)
+
+            enStart = (col * sqSize + offSet, row * sqSize + sqSize - offSet)
+            enEnd = (col * sqSize + sqSize - offSet, row * sqSize + offSet)
+            pygame.draw.line(screen, crossColor, enStart, enEnd, lineWidth)
+
+            pass
+
+        elif self.player == 2:
+            # draw circle
+            center = (col * sqSize + sqSize // 2, row * sqSize + sqSize // 2)
+            pygame.draw.circle(screen, circleColor, center, radius, lineWidth)
+
     def NextPlayer(self):
-        self.player = self.player % 2 +1
+        self.player = self.player % 2 + 1
+
 
 class main:
 
@@ -58,8 +127,9 @@ class main:
                     pos = event.pos
                     row = pos[1] // sqSize
                     col = pos[0] // sqSize
-                    if game.board.IsPlaceFree(row, col):
+                    if game.board.isPlaceFree(row, col):
                         game.board.mark_place(row, col, game.player)
+                        game.draw_fig(row, col)
                         game.NextPlayer()
                         print(game.board.place)
             pygame.display.update()
